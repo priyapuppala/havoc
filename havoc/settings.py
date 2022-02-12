396c6dac1f27cb1b9df68caa4add38825218fe81
priyapuppala <73129007+priyapuppala@users.noreply.github.com>
2022-02-12 10:22:52 +0530

    Update settings.py

--- havoc/settings.py
+++ havoc/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'havoc.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'havoc',
-        'USER': 'root',
-        'PASSWORD': 'trio',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
